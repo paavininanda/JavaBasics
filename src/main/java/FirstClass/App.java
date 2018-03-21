8cc91c6aa57138db2baba98e2fdd222429a55faf
paavininanda <paavininanda@gmail.com>
2018-03-21 22:58:21 +0530

    Added Task2, Adding two complex numbers

--- src/main/java/FirstClass/App.java
+++ src/main/java/FirstClass/App.java
@@ -1,13 +1,55 @@
 package FirstClass;
 
-/**
- * Hello world!
- *
- */
+import java.util.Scanner;
+
+class Reader
+{
+	private int[] inp = new int[4];
+	public int[] takeInput()
+	{
+		Scanner in = new Scanner(System.in);
+		for (int i = 0; i<4 ; i++)
+		{
+			if(i%2==0)
+				System.out.println("Enter number " + i);
+			inp[i] = in.nextInt();
+		}
+		return inp;
+	}
+}
+
+
+class Processor
+{
+	private int[] output = new int[2];
+	public int[] add (int[] input) 
+	{
+		for (int i=0;i<2;i++) 
+		{
+			output [i] = input[i] + input[i+2];
+		}
+		return output;
+	}
+}
+
+class Printer
+{
+	public void printSum (int[] output)
+	{
+		System.out.println(output[0] + output[1]+"i");
+	}
+}
+
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    		Reader r = new Reader();
+    		Processor p = new Processor();
+    		Printer pr = new Printer();
+    		int[] inputNums = r.takeInput();
+    		int[] outputNums = p.add(inputNums);
+    		pr.printSum(outputNums);
     }
 }
--- src/test/java/FirstClass/AppTest.java
+++ src/test/java/FirstClass/AppTest.java
@@ -1,38 +1,24 @@
 package FirstClass;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.*;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
-    extends TestCase
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+	@Test
+	public void AdditionOfTwoNumbersTesting() {
+	Processor p = new Processor();
+	int[] inputNums = new int[4];
+	inputNums[0] = 1;
+	inputNums[1] = 2;
+	inputNums[2] = 1;
+	inputNums[3] = 2;
+	int[] outputNums = p.add(inputNums);
+	assertEquals(outputNums[0],2);
+	assertEquals(outputNums[1],4);
+	}
 }
